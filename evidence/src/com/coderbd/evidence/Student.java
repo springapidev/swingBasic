12ba67502b4b347b807ef4b5281e62a39ddd7b79
Rezaul <springapidev@gmail.com>
2017-11-07 09:58:20 +0600

    evidence is for Advanced java

---
+++ evidence/src/com/coderbd/evidence/Student.java
@@ -0,0 +1,62 @@
+package com.coderbd.evidence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Rajaul Islam
+ */
+public class Student {
+    private String name;
+    private String email;
+    private int age;
+    private String gender;
+    private String hobby;
+    private String round;
+    private String note;
+
+    public Student() {
+    }
+
+    public Student(String name, String email, int age, String gender, String hobby, String round, String note) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.gender = gender;
+        this.hobby = hobby;
+        this.round = round;
+        this.note = note;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public String getRound() {
+        return round;
+    }
+
+    public String getNote() {
+        return note;
+    }
+    
+    
+    
+}
